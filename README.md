d4482041ba07bafbd83ab12556078d2e8359b96f
FireStreaker2 <michaelccc2019@outlook.com>
2023-07-05 20:41:14 -0700

    more cool stuff

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# undefined
-This project was generated using <a href="https://github.com/FireStreaker2/create-bad-app">create-bad-app</a>.
+
+# About
+create-bad-app is a CLI tool built with Node.js. It generates all the files needed to create a bad app, quickly and efficiently.
+
+# Notes
+create-bad-app automatically creates three files: ``index${extension}``, ``LICENSE``, and ``README.md``. The license file is automatically set to the MIT license, and the README file is a simple file saying that it was generated with create-bad-app. ``index${extension}`` however, is just a blank file.
+
+# Installation
+Via npm:
+```bash
+$ npm i -g create-bad-app
+```
+
+Via GitHub:
+```bash
+$ git clone https://github.com/FireStreaker2/create-bad-app.git
+$ cd create-bad-app
+$ npm i
+$ npm i -g .
+```
+
+# Usage
+Base syntax:
+```bash
+$ create-bad-app <directory name> <extension>
+```
+
+# License
+<a href="">MIT</a>
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ console.log(chalk.red("Generating Files...\n"));
 const currentDirectory = process.cwd();
 let newDirectoryName = process.argv[2];
 let directoryNameExists = true;
+const username = process.env.USER || os.userInfo().username;
+const extension = process.argv[3] || ".js";
 
 if (newDirectoryName) {
   const newDirectoryPath = path.join(currentDirectory, newDirectoryName);
@@ -21,8 +23,9 @@ if (newDirectoryName) {
 }
 
 const generateFiles = () => {
-  fs.writeFileSync("./LICENSE", `MIT License\n\nCopyright (c) ${new Date().getFullYear()} ${os.userInfo().username}\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.`);
+  fs.writeFileSync("./LICENSE", `MIT License\n\nCopyright (c) ${new Date().getFullYear()} ${username}\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.`);
   fs.writeFileSync("./README.md", `# ${newDirectoryName}\nThis project was generated using <a href="https://github.com/FireStreaker2/create-bad-app">create-bad-app</a>.`);
+  fs.writeFileSync(`index${extension}`, "");
 }
 
 generateFiles();
